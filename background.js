0726490e03d4b1dee0623fe3c2c32d53b24e47db
Masahiro <masahiro@nemut.ai>
2026-03-09 13:16:22 +0900

    Remove scripting permission

--- background.js
+++ background.js
@@ -1,29 +1,3 @@
-const TARGET_URLS = [
-  'https://x.com/*',
-  'https://twitter.com/*',
-];
-
-async function injectIntoOpenTabs() {
-  const tabs = await chrome.tabs.query({ url: TARGET_URLS });
-
-  for (const tab of tabs) {
-    if (!tab.id) continue;
-
-    try {
-      await chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        files: ['styles.css'],
-      });
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ['content.js'],
-      });
-    } catch (error) {
-      // Ignore tabs that are unloading or otherwise unavailable.
-    }
-  }
-}
-
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.storage.local.set({
@@ -31,12 +5,6 @@ chrome.runtime.onInstalled.addListener((details) => {
       settings: { showBlock: true, showMute: true },
     });
   }
-
-  injectIntoOpenTabs().catch(() => {});
-});
-
-chrome.runtime.onStartup.addListener(() => {
-  injectIntoOpenTabs().catch(() => {});
 });
 
 chrome.runtime.onMessage.addListener((message) => {
